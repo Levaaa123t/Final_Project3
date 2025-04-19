69209df8b4e7edcbbae088d871978a14d106c831
Levaaa123t <143887693+Levaaa123t@users.noreply.github.com>
2025-04-19 13:03:27 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,205 @@
+import telebot
+from telebot import types
+from telebot.types import BotCommand
+from config import *
+from logic import DB_questions
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
+
+default_questions = {
+    "Как оформить заказ?": "Для оформления заказа, пожалуйста, выберите интересующий вас товар и нажмите кнопку 'Добавить в корзину', затем перейдите в корзину и следуйте инструкциям для завершения покупки.",
+    "Как узнать статус моего заказа?": "Вы можете узнать статус вашего заказа, войдя в свой аккаунт на нашем сайте и перейдя в раздел 'Мои заказы'. Там будет указан текущий статус вашего заказа.",
+    "Как отменить заказ?": "Если вы хотите отменить заказ, пожалуйста, свяжитесь с нашей службой поддержки как можно скорее. Мы постараемся помочь вам с отменой заказа до его отправки.",
+    "Что делать, если товар пришел поврежденным?": "При получении поврежденного товара, пожалуйста, сразу свяжитесь с нашей службой поддержки и предоставьте фотографии повреждений. Мы поможем вам с обменом или возвратом товара.",
+    "Как связаться с вашей технической поддержкой?":"Вы можете связаться с нашей технической поддержкой через телефон на нашем сайте или написать нам в чат-бота.",
+    "Как узнать информацию о доставке?":"Информацию о доставке вы можете найти на странице оформления заказа на нашем сайте. Там указаны доступные способы доставки и сроки."
+}
+
+
+bot = telebot.TeleBot(TOKEN)
+db_commands = DB_questions()
+set_def_commands = [
+    BotCommand("start", "Запуск бота"),
+    BotCommand("faq", "Список часто задаваемых вопросов"),
+    BotCommand("questions", "Задать свой вопрос(/questions Ваш вопрос)")
+]
+
+set_admin_commands = [
+    BotCommand("start", "Запуск бота"),
+    BotCommand("faq", "Список часто задаваемых вопросов"),
+    BotCommand("questions_list", "Показать список вопросов которые ожидают ответа"),
+    BotCommand("questions_resolved","Показать список вопросов которые уже решены")
+]
+
+admin_id = admin_id
+bot.set_my_commands(set_def_commands)
+scope = types.BotCommandScopeChat(admin_id)
+bot.set_my_commands(set_admin_commands, scope=scope)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    check = db_commands.check_id(user_id=message.from_user.id )
+    if check:
+        bot.send_message(message.chat.id,f'Привет администратор {message.from_user.first_name}! Если хочешь ответить на вопросы пользователей используй команду /questions_list \nЕсли хочешь помочь мне используй команду /questions_resolved')
+    else:
+        bot.send_message(message.chat.id,'Привет я бот тех поддержка! Если у тебя есть вопросы то я могу помочь просто напиши /faq. Если я не смог тебе помочь обратись в тех поддержку с помощью команды /questions')
+
+@bot.message_handler(commands=['faq'])
+def faq_questions(message):
+    info = db_commands.get_list_faq()
+    if info:
+        text = "⁉️Часто задаваемые вопросы:\n" + "\n".join([f'❓ {x[0]} \n {x[1]}' for x in info])
+    else:
+        text = 'Ой что-то пошло не так попробуйте еще раз чуть позже'
+    
+    bot.send_message(message.chat.id, text)
+    
+@bot.message_handler(commands=['questions'])
+def question_user(message): 
+    user_question = message.text[10:].strip()
+    check = db_commands.check_id(user_id= message.from_user.id)
+    if check:
+        bot.send_message(message.chat.id, 'Вы являетесь администратором, а не пользователем!')
+    else:    
+        if user_question:
+            matched_answers = []  
+            user_words = set(user_question.lower().split()) 
+
+
+            for question, answer in default_questions.items():
+                question_words = set(question.lower().split())  
+                common_words = user_words & question_words  
+
+            
+                if len(common_words) >= 3:
+                    matched_answers.append(answer)
+
+            
+            if matched_answers:
+                for response in matched_answers:
+                    bot.send_message(message.chat.id, response)
+            else:
+                
+                bot.send_message(message.chat.id, 'К сожалению я не нашел ответ на ваш вопрос, перенаправляю его к специалистам.\nВозможно ответ на ваш вопрос есть здесь - /faq')
+                db_commands.save_user_question(user_id=message.from_user.id, question=user_question)
+        else:
+            bot.send_message(message.chat.id, "Пожалуйста укажите ваш вопрос после команды.")
+
+
+
+@bot.message_handler(commands=['questions_list'])
+def show_questions(message):
+    questions = db_commands.get_pending_questions() 
+    check = db_commands.check_id(user_id=message.from_user.id )
+    if check:
+        if not questions:
+            bot.send_message(message.chat.id,'Сейчас нет вопросов ожидающих ответа')
+            return
+        markup = InlineKeyboardMarkup()
+        for question in questions:
+            question_id = question[0] 
+            question_text = question[2]  
+            btn = InlineKeyboardButton(f"Вопрос {question_id}: {question_text[:30]}", callback_data=f"respond_question_{question_id}")
+            markup.add(btn)
+        bot.send_message(message.chat.id, "Выберите вопрос на который хотите ответить:", reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id,'У тебя нет доступа к этой функции!')
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith("respond_question_"))
+def handle_callback(call):
+    question_id = int(call.data.split("_")[2]) 
+    check = db_commands.check_id(user_id=call.from_user.id )
+    bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+    if check:
+        markup = InlineKeyboardMarkup()
+        but1 = InlineKeyboardButton("Ответить на вопрос", callback_data=f"respond_{question_id}")
+        markup.add(but1)
+        bot.send_message(call.message.chat.id, "Напишите ответ на вопрос:", reply_markup=markup)
+    else:
+        bot.send_message(call.message.chat.id,'У тебя нет доступа к этой функции!')
+
+admin_response = {}
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith("respond_"))
+def handle_respond_button(call):
+    bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+    question_id = int(call.data.split("_")[1])
+    admin_response[call.from_user.id] = question_id
+    bot.send_message(call.message.chat.id, "Введите текст ответа на этот вопрос:")
+
+@bot.message_handler(func=lambda message: message.from_user.id in admin_response)
+def handle_admin_reply(message):
+    markup = InlineKeyboardMarkup()
+    admin_id = message.from_user.id
+    question_id = admin_response.pop(admin_id)
+    answer = message.text
+    question = db_commands.get_id(question_id)
+    if question:
+        user_id = question[1]
+        but1 = InlineKeyboardButton("Вопрос не был решен", callback_data=f"actual_{question_id}")
+        but2 = InlineKeyboardButton("Вопрос решен", callback_data=f"resolved_{question_id}")
+        markup.add(but1, but2)
+        bot.send_message(user_id, f"Ответ на ваш вопрос:\n{answer}")
+        bot.send_message(user_id, "Был ли решен ваш вопрос?", reply_markup=markup)
+        bot.send_message(admin_id, "Ответ успешно отправлен пользователю")
+    else:
+        bot.send_message(admin_id, "Вопрос не найден.")
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith(('actual_', 'resolved_')))
+def update_status(call):
+    bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+    question_id = int(call.data.split('_')[1])
+    if call.data == f'resolved_{question_id}': 
+        db_commands.update_question_status(status='Решен', question_id=question_id)
+        bot.answer_callback_query(callback_query_id=call.id, text = f"Статус вопроса изменен на Решен", show_alert = False)
+        bot.send_message(call.message.chat.id, f"Вопрос решен")
+    else:
+        db_commands.update_question_status(status='Ожидание', question_id=question_id)
+        bot.answer_callback_query(callback_query_id=call.id, text = 'Я оставил статус вопроса на ожидание', show_alert = False)
+        bot.send_message(call.message.chat.id,'Статус вашего вопроса остается "ожидающие ответа поддержки", ожидайте ответа поддержки')
+
+@bot.message_handler(commands=['questions_resolved'])
+def resolved_questions(message):
+    resolv_questions = db_commands.get_resolved_questions() 
+    check = db_commands.check_id(user_id=message.from_user.id )
+    if check:
+        if not resolv_questions:
+            bot.send_message(message.chat.id,'В данный момент все вопросы ожидают вашего ответа')
+            return
+        markup = InlineKeyboardMarkup()
+        for del_question in resolv_questions:
+            del_question_id = del_question[0] 
+            del_question_text = del_question[1]  
+            btn = InlineKeyboardButton(f"Вопрос {del_question_id}: {del_question_text[:30]}", callback_data=f"choose_question_{del_question_id}")
+            markup.add(btn)
+        bot.send_message(message.chat.id, "Выберите вопрос для удаления:", reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id,'У тебя нет доступа к этой функции!')
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith("choose_question_"))
+def del_callback(call):
+    bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+    question_id = int(call.data.split("_")[2]) 
+    check = db_commands.check_id(user_id=call.from_user.id )
+    if check:
+        markup = InlineKeyboardMarkup()
+        but1 = InlineKeyboardButton("Удалить вопрос", callback_data=f"delete_{question_id}")
+        markup.add(but1)
+        bot.send_message(call.message.chat.id, "Секунду удаляю вопрос:", reply_markup=markup)
+    else:
+        bot.send_message(call.message.chat.id,'У тебя нет доступа к этой функции!')
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith(('delete_')))
+def delete_question(call):
+    bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+    question_id = int(call.data.split('_')[1])
+    if call.data == f'delete_{question_id}': 
+        db_commands.delete_user_question(question_id = question_id)
+        bot.answer_callback_query(callback_query_id=call.id, text = f"Вопрос #{question_id} был удален", show_alert = False)
+        bot.send_message(call.message.chat.id, f"Вопрос #{question_id} был удален")
+    else:
+        bot.send_message(call.message.chat.id, 'Что то пошло не так')
+
+
+
+bot.infinity_polling()
---
+++ logic.py
@@ -0,0 +1,170 @@
+import sqlite3
+from config import DB_NAME
+
+# Предустановленные вопросы и ответы
+default_questions = [
+    ("Как оформить заказ?", "Для оформления заказа, пожалуйста, выберите интересующий вас товар и нажмите кнопку 'Добавить в корзину', затем перейдите в корзину и следуйте инструкциям для завершения покупки."),
+    ("Как узнать статус моего заказа?", "Вы можете узнать статус вашего заказа, войдя в свой аккаунт на нашем сайте и перейдя в раздел 'Мои заказы'. Там будет указан текущий статус вашего заказа."),
+    ("Как отменить заказ?", "Если вы хотите отменить заказ, пожалуйста, свяжитесь с нашей службой поддержки как можно скорее. Мы постараемся помочь вам с отменой заказа до его отправки."),
+    ("Что делать, если товар пришел поврежденным?", "При получении поврежденного товара, пожалуйста, сразу свяжитесь с нашей службой поддержки и предоставьте фотографии повреждений. Мы поможем вам с обменом или возвратом товара."),
+    ("Как связаться с вашей технической поддержкой?","Вы можете связаться с нашей технической поддержкой через телефон на нашем сайте или написать нам в чат-бота."),
+    ("Как узнать информацию о доставке?","Информацию о доставке вы можете найти на странице оформления заказа на нашем сайте. Там указаны доступные способы доставки и сроки.")
+]
+
+class DB_questions:
+    def __init__(self, database=DB_NAME):
+        self.database = database
+        self.create_tables()
+        self.default_insert()
+
+    def create_tables(self):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute('''CREATE TABLE IF NOT EXISTS questions (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                question TEXT UNIQUE NOT NULL,
+                                answer TEXT NOT NULL
+                              )''')
+
+            cursor.execute('''CREATE TABLE IF NOT EXISTS user_questions (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                user_id INTEGER,
+                                question TEXT NOT NULL,
+                                status TEXT DEFAULT 'Ожидание' 
+                              )''')
+            cursor.execute('''CREATE TABLE IF NOT EXISTS admins( 
+                           id INTEGER PRIMARY KEY AUTOINCREMENT,
+                           user_id INTEGER
+                           )  ''')
+            conn.commit()
+
+    def __executemany(self, sql, data):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            conn.executemany(sql, data)
+            conn.commit()
+    def __select_data(self, sql, data = tuple()):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute(sql, data)
+            return cur.fetchall()
+
+    def default_insert(self):
+        sql = 'INSERT OR IGNORE INTO questions (question, answer) VALUES (?, ?)'
+        self.__executemany(sql, default_questions)
+
+    def default_admins(self):
+        sql = 'INSERT OR IGNORE INTO admins (user_id) VALUES (?)'
+        data = [(admin_id,)]
+        self.__executemany(sql, data)
+
+    # def get_answer(self, user_question):
+    #     """Автоматически отвечает на часто задаваемые вопросы"""
+    #     conn = sqlite3.connect(self.database)
+    #     cursor = conn.cursor()
+    #     words = user_question.split()
+    #     if len(words) <2:
+    #         return None
+    #     conditions = [f"question LIKE ?" for _ in words]
+    #     sql = "SELECT answer FROM questions WHERE " + " OR ".join(conditions)
+    #     parameters = [f"%{word}%" for word in words]
+    #     cursor.execute(sql, parameters)
+    #     answer = cursor.fetchone()
+    #     if answer:
+    #         return answer[0]
+    #     else:
+    #         return None
+    
+    def get_answer(self, user_question):
+        conn = sqlite3.connect(self.database)
+        cursor = conn.cursor()
+        words = user_question.split()
+        
+        if len(words) < 2:
+            return None
+        
+        conditions = [f"question LIKE ?" for _ in words]
+        sql = "SELECT answer FROM questions WHERE " + " OR ".join(conditions)
+        parameters = [f"%{word}%" for word in words]
+        
+        cursor.execute(sql, parameters)
+        answer = cursor.fetchone()
+        if answer:
+            return answer[0]
+        else:
+            return None
+        # cursor.execute("SELECT answer FROM questions WHERE question = ?", (user_question,))
+        # answer = cursor.fetchone()
+        # return answer[0] if answer else None
+
+    def save_user_question(self, user_id, question):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO user_questions (user_id, question, status) VALUES (?, ?, 'Ожидание')", (user_id, question))
+            conn.commit()
+
+    def get_pending_questions(self):
+        conn = sqlite3.connect(self.database)
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, user_id, question FROM user_questions WHERE status = 'Ожидание'")
+        return cursor.fetchall()
+
+    def update_question_status(self, question_id, status):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute("UPDATE user_questions SET status = ? WHERE id = ?", (status, question_id))
+            conn.commit()
+
+    def clear_tables(self):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM questions") 
+            cursor.execute("DELETE FROM user_questions")  
+            conn.commit()
+
+    def delete_user_question(self, question_id):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM user_questions WHERE id = ? AND status = 'Решен'", (question_id,))
+            conn.commit()
+    def get_list_faq(self):
+        sql = "SELECT question, answer FROM questions"
+        return self.__select_data(sql)
+
+    def check_id(self, user_id):
+        conn = sqlite3.connect(self.database)
+        cursor = conn.cursor()
+        cursor.execute('SELECT 1 FROM admins WHERE user_id = ?', (user_id,))
+        result = cursor.fetchone()
+        conn.close()
+        return result is not None
+    
+    def get_questions_pending(self):
+        sql = 'SELECT id, question FROM user_questions'
+        return self.__select_data(sql)
+    
+    def get_resolved_questions(self):
+        sql = "SELECT id, question FROM user_questions WHERE status = 'Решен' "
+        return self.__select_data(sql)
+    
+    def get_id(self, question_id):
+        conn = sqlite3.connect(self.database)
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM user_questions WHERE id = ?', (question_id,))
+        result = cursor.fetchone()
+        conn.close()
+        return result
+        
+
+if __name__ == '__main__':
+    manager = DB_questions(DB_NAME)
+    manager.default_insert()
+    #manager.default_admins()
+    #manager.clear_tables()
+    #manager.get_list_faq()
